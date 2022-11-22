e0e3316b32436df482a9619b5a015636ed8dd93c
buddyxiao <1464078415@qq.com>
2022-11-22 21:43:21 +0800

    add text库

--- main.go
+++ main.go
@@ -1,9 +1,11 @@
 package main
 
 import (
+	"bytes"
 	"fmt"
 	"io/ioutil"
 	"net/http"
+	"strings"
 )
 
 func main() {
@@ -23,5 +25,14 @@ func main() {
 		fmt.Printf("read content failed:%v", err)
 		return
 	}
-	fmt.Println("body:", string(body))
+	numLinks := strings.Count(string(body), "<a")
+	fmt.Printf("homepage has %d links!\n", numLinks)
+	exist := strings.Contains(string(body), "疫情")
+	fmt.Printf("是否存在疫情:%v\n", exist)
+
+	numLinks = bytes.Count(body, []byte("<a"))
+	fmt.Printf("homepage has %d links!\n", numLinks)
+
+	exist = bytes.Contains(body, []byte("疫情"))
+	fmt.Printf("是否存在疫情:%v\n", exist)
 }

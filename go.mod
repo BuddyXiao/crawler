7560b3d5f149080e3fd7f1fccab54fb388533c0c
buddyxiao <1464078415@qq.com>
2022-11-23 22:11:33 +0800

    goquery

--- go.mod
+++ go.mod
@@ -3,6 +3,8 @@ module github.com/buddyxiao/crawler
 go 1.18
 
 require (
+	github.com/PuerkitoBio/goquery v1.8.0 // indirect
+	github.com/andybalholm/cascadia v1.3.1 // indirect
 	github.com/antchfx/htmlquery v1.2.5 // indirect
 	github.com/antchfx/xpath v1.2.1 // indirect
 	github.com/golang/groupcache v0.0.0-20200121045136-8c9f03a8e57e // indirect
--- main.go
+++ main.go
@@ -1,59 +1,27 @@
 package main
 
 import (
-	"bufio"
 	"bytes"
 	"fmt"
-	"github.com/antchfx/htmlquery"
-	"golang.org/x/net/html/charset"
-	"golang.org/x/text/encoding"
-	"golang.org/x/text/encoding/unicode"
-	"golang.org/x/text/transform"
-	"io/ioutil"
-	"net/http"
-	"regexp"
+	"github.com/PuerkitoBio/goquery"
+	"github.com/buddyxiao/crawler/collect"
+	"time"
 )
-
-var headerRe = regexp.MustCompile(`<div class="small_toplink__GmZhY"[\s\S]*?<h2>([\s\S]*?)</h2>`)
 
 func main() {
 	url := "https://www.thepaper.cn/"
-	body, err := Fetch(url)
+	var f collect.Fetcher = collect.BrowserFetch{Timeout: 3 * time.Second}
+	body, err := f.Get(url)
 	if err != nil {
-		fmt.Println("fetch err:", err)
+		fmt.Println("read content failed:%v", err)
 		return
 	}
-	doc, err := htmlquery.Parse(bytes.NewReader(body))
+	doc, err := goquery.NewDocumentFromReader(bytes.NewReader(body))
 	if err != nil {
-		fmt.Printf("htmlquery.Parse failed:%v", err)
+		fmt.Printf("read content failed:%v", err)
 	}
-	nodes := htmlquery.Find(doc, `//div[@class="small_toplink__GmZhY"]/a/h2`)
-	for _, node := range nodes {
-		fmt.Println("fetch data:", node.FirstChild.Data)
-	}
-}
-
-func Fetch(url string) ([]byte, error) {
-	resp, err := http.Get(url)
-	if err != nil {
-		panic(err)
-	}
-	defer resp.Body.Close()
-	if resp.StatusCode != http.StatusOK {
-		fmt.Printf("Error status code: %v", resp.StatusCode)
-	}
-	bodyReader := bufio.NewReader(resp.Body)
-	e := DetermineEncoding(bodyReader)
-	utf8Reader := transform.NewReader(bodyReader, e.NewDecoder())
-	return ioutil.ReadAll(utf8Reader)
-}
-
-func DetermineEncoding(r *bufio.Reader) encoding.Encoding {
-	bytes, err := r.Peek(1024)
-	if err != nil {
-		fmt.Println("fetch error", err)
-		return unicode.UTF8
-	}
-	e, _, _ := charset.DetermineEncoding(bytes, "")
-	return e
+	doc.Find("div.small_cardcontent__BTALp a h2").Each(func(i int, s *goquery.Selection) {
+		content := s.Text()
+		fmt.Println(i, ":", content)
+	})
 }
